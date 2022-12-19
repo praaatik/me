56793f603a21b832f3cce8ac40d3385fba17cefc
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2022-12-19 21:11:46 +0530

    Style table of contents.

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -15,20 +15,24 @@ const TableOfContents = ({ contents }: ITableOfContentArray) => {
   return (
     <div>
       <div className="lg:mb-4 lg:text-xl lg:text-left">Table of contents</div>
-      <div className="mb-4 mt-0 mx-2 italic border-2 border-black rounded-lg overflow-y-auto h-screen text-sm py-2">
-        <ul>
+      <div className="mb-4 mt-0 mx-2 italic rounded-lg overflow-y-auto h-[85vh] text-sm py-2 leading-8 scroll-">
+        <ul className="pb-4">
           {contents.map((content) => {
             return (
               <div
                 key={content?.url}
-                className={content?.level === 3 ? "px-10" : "px-1"}
+                className={
+                  content?.level === 3
+                    ? "px-10 relative hover:underline"
+                    : "px-1 relative hover:underline"
+                }
               >
                 <li
                   key={content?.url}
                   className={
                     content?.level === 3
-                      ? "list-square list-inside"
-                      : "list-disc list-inside"
+                      ? "before:content-[''] before:block before:h-10 before:w-[1px] before:bg-slate-600 before:left-5 before:absolute before:overflow-hidden"
+                      : ""
                   }
                 >
                   <Link href={content.url} key={content.url}>
