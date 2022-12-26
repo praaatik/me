138158c10f3cff2c785ca408ca106418ac48aef9
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2022-12-26 09:28:02 +0530

    Add theme for the load more posts button.

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,33 @@
 import { getAllPosts } from "@/src/api";
 import Posts from "@/src/components/Posts";
 import { PostMetadata } from "@/src/interfaces/PostMetadata";
-import React, { useState } from "react";
+import { ThemeContext } from "pages/_app";
+import React, { useContext, useState } from "react";
 
 export default function Blog({ posts }: { posts: PostMetadata[] }) {
   const [currentIndex, currentIndexSet] = useState(0);
+  const context = useContext(ThemeContext);
+
+  interface ILoadPostsButtonStyles {
+    allowedStyles: string;
+    nowAllowedStyles: string;
+  }
+
+  const lightStyles: ILoadPostsButtonStyles = {
+    nowAllowedStyles:
+      "bg-light-background-1 hover:bg-light-background-1 text-dark-background-1 font-semibold py-2 px-4 border-2 border-light-sea rounded shadow hover:cursor-not-allowed mb-4 ",
+
+    allowedStyles:
+      "bg-light-background-1 hover:bg-light-background-3 text-dark-background-1 font-semibold py-2 px-4 border-2 border-light-sea rounded shadow mb-4",
+  };
+
+  const darkStyles: ILoadPostsButtonStyles = {
+    nowAllowedStyles:
+      "bg-dark-background-1 hover:bg-dark-background-1 text-light-background-1 font-semibold py-2 px-4 border border-light-sea rounded shadow hover:cursor-not-allowed mb-4 ",
+
+    allowedStyles:
+      "bg-dark-background-1 hover:bg-dark-background-3 text-light-background-1 font-semibold py-2 px-4 border border-light-sea rounded shadow mb-4",
+  };
 
   return (
     <>
@@ -13,9 +36,13 @@ export default function Blog({ posts }: { posts: PostMetadata[] }) {
         <button
           onClick={() => currentIndexSet(currentIndex + 10)}
           className={
-            currentIndex > posts?.length
-              ? "bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow hover:cursor-not-allowed mb-4 "
-              : "bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mb-4"
+            context?.isThemeDark
+              ? currentIndex > posts?.length
+                ? darkStyles?.nowAllowedStyles
+                : darkStyles?.allowedStyles
+              : currentIndex > posts?.length
+              ? lightStyles?.nowAllowedStyles
+              : lightStyles?.allowedStyles
           }
         >
           Load more posts
